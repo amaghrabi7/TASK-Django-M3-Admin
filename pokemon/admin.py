7e618ab8c2948fad0dad28e9425bfce00c3a32a3
amaghrabi7 <abdul.maghrabi@gmail.com>
2022-10-11 20:00:03 +0300

    Fixed name_jp in model

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Pokemon
 
 # Register your models here.
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "hp", "active")
+    list_filter = ("active",)
+    list_display_links = ("id", "name")
